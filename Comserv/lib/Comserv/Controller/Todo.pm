fdcb14536d2c990d83422c6f6d41b0826261cd56
shantamcbain <shantamcbain>
2024-02-18 06:49:30 -0800

    removed the finished todo from list on todo page.

--- Comserv/lib/Comserv/Controller/Todo.pm
+++ Comserv/lib/Comserv/Controller/Todo.pm
@@ -26,7 +26,7 @@ sub todo :Path('/todo') :Args(0) {
 
     # Fetch todos for the site, ordered by start_date
     my @todos = $rs->search(
-        { sitename => $c->session->{SiteName} },  # filter by site
+        { sitename => $c->session->{SiteName}, status => { '!=' => 3 }  },  # filter by site
         { order_by => 'start_date' }  # order by start_date
     );
 
