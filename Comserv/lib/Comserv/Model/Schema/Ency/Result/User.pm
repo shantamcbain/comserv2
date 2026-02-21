1c5481c8b6f92346d288ffc5249cea9bf55feda0
Comserv Admin <admin@comserv.com>
2026-02-21 09:00:19 -0800

    Enhance Login with Email/Username Support
    The `roles` field also needs to be nullable or have a default value. Let me update the User Result:

--- Comserv/lib/Comserv/Model/Schema/Ency/Result/User.pm
+++ Comserv/lib/Comserv/Model/Schema/Ency/Result/User.pm
@@ -35,6 +35,7 @@ __PACKAGE__->add_columns(
     roles => {
         data_type => 'text',
         is_nullable => 0,
+        default_value => 'user',
     },
     email_notifications => {
         data_type => 'tinyint',
