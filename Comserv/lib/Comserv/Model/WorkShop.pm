53bcc6408d132e93da1aa3306b558a6f96a95bab
Comserv Admin <admin@comserv.com>
2026-02-27 06:16:45 -0800

    Fix workshop visibility; fix Resources.tt size calc; add NFS scan-to-files-DB action

--- Comserv/lib/Comserv/Model/WorkShop.pm
+++ Comserv/lib/Comserv/Model/WorkShop.pm
@@ -5,240 +5,60 @@ use Comserv::Util::AdminAuth;
 
 extends 'Catalyst::Model';
 
-# In Model/WorkShop.pm
 sub get_active_workshops {
     my ($self, $c) = @_;
 
-    # Get a DBIx::Class::Schema object
-    my $schema = $c->model('DBEncy');
-
-    # Get a DBIx::Class::ResultSet object for the 'WorkShop' table
-    my $rs = $schema->resultset('WorkShop');
-
+    my $rs = $c->model('DBEncy')->resultset('WorkShop');
     my @workshops;
     my $error;
     eval {
         my $admin_auth = Comserv::Util::AdminAuth->new();
         my $admin_type = $admin_auth->get_admin_type($c);
-        my $sitename = $c->session->{SiteName};
-        my $user_id = $c->session->{user_id};
-        my $roles = $c->session->{roles} || [];
-        
-        # Check if user is workshop leader
-        my $is_workshop_leader = 0;
-        if (ref $roles eq 'ARRAY') {
-            $is_workshop_leader = grep { $_ eq 'workshop_leader' } @$roles;
-        }
-        
-        # CSC admin (god-level) sees all non-draft workshops in the public listing.
-        # Drafts are only visible in the Dashboard.
-        if ($admin_type eq 'csc' || $admin_type eq 'special') {
-            @workshops = $rs->search(
-                {
-                    'me.date'   => { '>=' => DateTime->today->ymd },
-                    'me.status' => { '!=' => 'draft' },
-                },
-                { 
-                    order_by => { -asc => 'me.date' },
-                    prefetch => 'creator'
-                }
-            );
-        } elsif ($admin_type eq 'standard' || $is_workshop_leader) {
-            my $site_id;
-            if ($sitename) {
-                my $site = $schema->resultset('Site')->search({ name => $sitename })->first;
-                $site_id = $site->id if $site;
-            }
-            
-            my $search_filter = {
-                'me.date' => { '>=' => DateTime->today->ymd },
-            };
-            
-            if ($site_id) {
-                $search_filter->{-or} = [
-                    { 'me.share' => 'public', 'me.status' => 'published' },
-                    { 'site_associations.site_id' => $site_id, 'me.status' => 'published' },
-                    { 'me.created_by' => $user_id }
-                ];
-            } else {
-                $search_filter->{-or} = [
-                    { 'me.share' => 'public', 'me.status' => 'published' },
-                    { 'me.created_by' => $user_id }
-                ];
-            }
-            
-            @workshops = $rs->search(
-                $search_filter,
-                { 
-                    order_by => { -asc => 'me.date' },
-                    prefetch => 'creator',
-                    join => 'site_associations',
-                    distinct => 1
-                }
-            );
-        } else {
-            # Regular users see only published workshops
-            my $site_id;
-            if ($sitename) {
-                my $site = $schema->resultset('Site')->search({ name => $sitename })->first;
-                $site_id = $site->id if $site;
-            }
-            
-            my $search_filter = {
-                'me.date'   => { '>=' => DateTime->today->ymd },
-                'me.status' => 'published',
-            };
-            
-            if ($site_id) {
-                $search_filter->{-or} = [
-                    { 'me.share' => 'public' },
-                    { 'site_associations.site_id' => $site_id }
-                ];
-            } else {
-                $search_filter->{'me.share'} = 'public';
-            }
-            
-            @workshops = $rs->search(
-                $search_filter,
-                { 
-                    order_by => { -asc => 'me.date' },
-                    prefetch => 'creator',
-                    join => 'site_associations',
-                    distinct => 1
-                }
-            );
-        }
-    };
-    if ($@) {
-        $error = "Error fetching active workshops: $@";
-    }
+        my $is_admin   = ($admin_type eq 'csc' || $admin_type eq 'special' || $admin_type eq 'standard');
 
+        my $filter = { 'me.date' => { '>=' => DateTime->today->ymd } };
+
+        # Admins see all non-draft active workshops; everyone else sees only published
+        $filter->{'me.status'} = $is_admin ? { '!=' => 'draft' } : 'published';
+
+        @workshops = $rs->search(
+            $filter,
+            { order_by => { -asc => 'me.date' }, prefetch => 'creator' }
+        );
+    };
+    $error = "Error fetching active workshops: $@" if $@;
     return (\@workshops, $error);
 }
+
 sub get_workshop_by_id {
     my ($self, $c, $id) = @_;
-
-    # Get a DBIx::Class::Schema object
-    my $schema = $c->model('DBEncy');
-
-    # Get a DBIx::Class::ResultSet object for the 'WorkShop' table
-    my $rs = $schema->resultset('WorkShop');
-
-    # Try to find the workshop by its ID
     my $workshop;
-    eval {
-        $workshop = $rs->find($id);
-    };
-    if ($@) {
-        return (undef, "Error fetching workshop: $@");
-    }
-
-    return ($workshop, undef);
+    eval { $workshop = $c->model('DBEncy')->resultset('WorkShop')->find($id) };
+    return $@ ? (undef, "Error fetching workshop: $@") : ($workshop, undef);
 }
 
 sub get_past_workshops {
     my ($self, $c) = @_;
 
-    my $schema = $c->model('DBEncy');
-    my $rs = $schema->resultset('WorkShop');
-
+    my $rs = $c->model('DBEncy')->resultset('WorkShop');
     my @workshops;
     my $error;
     eval {
         my $admin_auth = Comserv::Util::AdminAuth->new();
         my $admin_type = $admin_auth->get_admin_type($c);
-        my $sitename = $c->session->{SiteName};
-        my $user_id = $c->session->{user_id};
-        my $roles = $c->session->{roles} || [];
-        
-        # Check if user is workshop leader
-        my $is_workshop_leader = 0;
-        if (ref $roles eq 'ARRAY') {
-            $is_workshop_leader = grep { $_ eq 'workshop_leader' } @$roles;
-        }
-        
-        # CSC admin sees all non-draft past workshops in the public listing.
-        # Drafts are only visible in the Dashboard.
-        if ($admin_type eq 'csc' || $admin_type eq 'special') {
-            @workshops = $rs->search(
-                {
-                    'me.date'   => { '<' => DateTime->today->ymd },
-                    'me.status' => { '!=' => 'draft' },
-                },
-                { 
-                    order_by => { -desc => 'me.date' },
-                    prefetch => 'creator'
-                }
-            );
-        } elsif ($admin_type eq 'standard' || $is_workshop_leader) {
-            my $site_id;
-            if ($sitename) {
-                my $site = $schema->resultset('Site')->search({ name => $sitename })->first;
-                $site_id = $site->id if $site;
-            }
-            
-            my $search_filter = {
-                'me.date'   => { '<' => DateTime->today->ymd },
-                'me.status' => { '!=' => 'draft' },
-            };
-            
-            if ($site_id) {
-                $search_filter->{-or} = [
-                    { 'me.share' => 'public', 'me.status' => 'published' },
-                    { 'site_associations.site_id' => $site_id, 'me.status' => 'published' },
-                ];
-            } else {
-                $search_filter->{-or} = [
-                    { 'me.share' => 'public', 'me.status' => 'published' },
-                ];
-            }
-            
-            @workshops = $rs->search(
-                $search_filter,
-                { 
-                    order_by => { -desc => 'me.date' },
-                    prefetch => 'creator',
-                    join => 'site_associations',
-                    distinct => 1
-                }
-            );
-        } else {
-            # Regular users see only published workshops
-            my $site_id;
-            if ($sitename) {
-                my $site = $schema->resultset('Site')->search({ name => $sitename })->first;
-                $site_id = $site->id if $site;
-            }
-            
-            my $search_filter = {
-                'me.date'   => { '<' => DateTime->today->ymd },
-                'me.status' => 'published',
-            };
-            
-            if ($site_id) {
-                $search_filter->{-or} = [
-                    { 'me.share' => 'public' },
-                    { 'site_associations.site_id' => $site_id }
-                ];
-            } else {
-                $search_filter->{'me.share'} = 'public';
-            }
-            
-            @workshops = $rs->search(
-                $search_filter,
-                { 
-                    order_by => { -desc => 'me.date' },
-                    prefetch => 'creator',
-                    join => 'site_associations',
-                    distinct => 1
-                }
-            );
-        }
-    };
-    if ($@) {
-        $error = "Error fetching past workshops: $@";
-    }
+        my $is_admin   = ($admin_type eq 'csc' || $admin_type eq 'special' || $admin_type eq 'standard');
 
+        my $filter = { 'me.date' => { '<' => DateTime->today->ymd } };
+
+        # Admins see all non-draft past workshops; everyone else sees only published
+        $filter->{'me.status'} = $is_admin ? { '!=' => 'draft' } : 'published';
+
+        @workshops = $rs->search(
+            $filter,
+            { order_by => { -desc => 'me.date' }, prefetch => 'creator' }
+        );
+    };
+    $error = "Error fetching past workshops: $@" if $@;
     return (\@workshops, $error);
 }
 
