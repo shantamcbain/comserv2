504056301ed4dafd90b93981f1104f63d41f5134
Comserv Admin <admin@comserv.com>
2026-04-10 14:44:13 -0700

    fix: add DBIC stubs to deprecated Bee* Result classes; make Email::MIME and IPC::Run optional

--- Comserv/lib/Comserv/Model/Schema/Ency/Result/BeeEquipmentType.pm
+++ Comserv/lib/Comserv/Model/Schema/Ency/Result/BeeEquipmentType.pm
@@ -13,5 +13,12 @@ package Comserv::Model::Schema::Ency::Result::BeeEquipmentType;
 
 use strict;
 use warnings;
+use base 'DBIx::Class::Core';
+
+__PACKAGE__->table('bee_equipment_types');
+__PACKAGE__->add_columns(
+    id => { data_type => 'integer', is_auto_increment => 1, is_nullable => 0 },
+);
+__PACKAGE__->set_primary_key('id');
 
 1;
