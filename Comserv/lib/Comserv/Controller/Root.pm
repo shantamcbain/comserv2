5f72cb1bb7ee0264eb1db0c8a491975535812913
Comserv Admin <admin@comserv.com>
2026-04-20 12:08:32 -0700

    Root: port 4001 favicon label → 'Pl' (PlanningSystem)

--- Comserv/lib/Comserv/Controller/Root.pm
+++ Comserv/lib/Comserv/Controller/Root.pm
@@ -1909,7 +1909,7 @@ sub begin :Private {
 
 sub _port_label {
     my ($port) = @_;
-    my %named = ( 4010 => 'AI', 4020 => 'DEV', 4030 => 'QA' );
+    my %named = ( 4001 => 'Pl', 4010 => 'AI', 4020 => 'DEV', 4030 => 'QA' );
     return $named{$port} if exists $named{$port};
     my $s = "$port";
     $s =~ s/0+$// if $s =~ /0+$/;
