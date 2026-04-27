e5a7a1c8a1eb1763848c1e4f2ed175a24cf18382
Comserv Admin <admin@comserv.com>
2026-04-27 05:23:28 -0700

    Fix todo edit/details status string/numeric mismatch and project dropdown depth

--- Comserv/lib/Comserv/Controller/Project.pm
+++ Comserv/lib/Comserv/Controller/Project.pm
@@ -509,12 +509,29 @@ sub fetch_projects_with_subprojects :Private {
             };
             
             foreach my $subproject1 (@level1_subprojects) {
+                my @level2_subprojects;
+                eval {
+                    @level2_subprojects = $schema->resultset('Project')->search(
+                        { parent_id => $subproject1->id },
+                        { order_by => { -asc => 'name' } }
+                    )->all;
+                };
+                my @level2_hashes;
+                foreach my $subproject2 (@level2_subprojects) {
+                    push @level2_hashes, {
+                        id           => $subproject2->id,
+                        name         => $subproject2->name,
+                        parent_id    => $subproject2->parent_id,
+                        status       => $subproject2->status || 1,
+                        sub_projects => []
+                    };
+                }
                 push @{$project_hash->{sub_projects}}, {
-                    id => $subproject1->id,
-                    name => $subproject1->name,
-                    parent_id => $subproject1->parent_id,
-                    status => $subproject1->status || 1,
-                    sub_projects => []
+                    id           => $subproject1->id,
+                    name         => $subproject1->name,
+                    parent_id    => $subproject1->parent_id,
+                    status       => $subproject1->status || 1,
+                    sub_projects => \@level2_hashes
                 };
             }
         }
